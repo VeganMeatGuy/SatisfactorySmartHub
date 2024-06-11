3679ffdcd63ada25646524824902cea15d2fefda
Peter Steudel <peter.steudel@siemens.com>
2024-06-11 15:25:58 +0200

    feature: added list of recipes to RecipeModelService, changed Tests -not ready

--- SatisfactoryCalculator/Application/Services/ProductionLineModelService.cs
+++ SatisfactoryCalculator/Application/Services/ProductionLineModelService.cs
@@ -12,53 +12,53 @@ internal class ProductionLineModelService(RecipeModelService recipeModelService)
 {
     public List<ProductionLineModel> GetProductionLinesForItem(ItemModel model)
     {
-        ICollection<ProductionLineModel> openProductionLines = new HashSet<ProductionLineModel>();
+        //ICollection<ProductionLineModel> openProductionLines = new HashSet<ProductionLineModel>();
         ICollection<ProductionLineModel> finishedProductionLines = new HashSet<ProductionLineModel>();
 
-        // suche die Rezepte für das gegebene Model
-        ICollection<RecipeModel> recipes = recipeModelService.GetMainRecipes(model);
+        //// suche die Rezepte für das gegebene Model
+        //ICollection<RecipeModel> recipes = recipeModelService.GetMainRecipes(model);
 
-        //je Rezept wird ein ProductionLineModel & ein ProcessStepModel angelegt
-        foreach (RecipeModel recipe in recipes)
-        {
-            ProcessStepModel processStepModel = new();
-            processStepModel.Recipe = recipe;
-            ProductionLineModel productionLine = new();
-            productionLine.ProcessSteps.Add(processStepModel);
-            openProductionLines.Add(productionLine);
-        }
+        ////je Rezept wird ein ProductionLineModel & ein ProcessStepModel angelegt
+        //foreach (RecipeModel recipe in recipes)
+        //{
+        //    ProcessStepModel processStepModel = new();
+        //    processStepModel.Recipe = recipe;
+        //    ProductionLineModel productionLine = new();
+        //    productionLine.ProcessSteps.Add(processStepModel);
+        //    openProductionLines.Add(productionLine);
+        //}
 
-        while (openProductionLines.Count > 0)
-        {
-            ProductionLineModel activeProdLine = openProductionLines.First();
+        //while (openProductionLines.Count > 0)
+        //{
+        //    ProductionLineModel activeProdLine = openProductionLines.First();
 
-            var possipleLastSteps = activeProdLine.ProcessSteps.Where(x => x.PostStep == null);
+        //    var possipleLastSteps = activeProdLine.ProcessSteps.Where(x => x.PostStep == null);
 
-            if (possipleLastSteps.Count() > 1)
-                throw new Exception();
+        //    if (possipleLastSteps.Count() > 1)
+        //        throw new Exception();
 
-            ProcessStepModel lastStep = possipleLastSteps.First();
+        //    ProcessStepModel lastStep = possipleLastSteps.First();
 
-            //finde die Zutaten für den letzten Schritt ein ProcessStep
+        //    //finde die Zutaten für den letzten Schritt ein ProcessStep
 
-            foreach (ItemWithAmount Zutat in lastStep.Recipe.Ingredients)
-            {
+        //    foreach (ItemWithAmount Zutat in lastStep.Recipe.Ingredients)
+        //    {
 
-            }
-        }
+        //    }
+        //}
 
 
 
-            //jetzt habe ich eine production Line mit einem Prozessschritt
-            //nun muss ich der produktionslinie noch die restlichen Prozessschritte hinzufügen
+        //    //jetzt habe ich eine production Line mit einem Prozessschritt
+        //    //nun muss ich der produktionslinie noch die restlichen Prozessschritte hinzufügen
 
             
-        // für jedes ProcessStepModel wird in den Zutaten geschaut, und je zutat nach dem rezept gesucht
+        //// für jedes ProcessStepModel wird in den Zutaten geschaut, und je zutat nach dem rezept gesucht
 
 
 
 
-        return productionLines.ToList();
+        return finishedProductionLines.ToList();
     }
 
 }
