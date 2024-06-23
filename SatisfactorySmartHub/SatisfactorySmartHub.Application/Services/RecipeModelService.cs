d4a6adae89acd66e93e87afa6c595e0b677255de
VeganMeatGuy <steudel.peter@gmail.com>
2024-06-23 18:17:45 +0200

    change Solution
    everthing new

---
+++ SatisfactorySmartHub/SatisfactorySmartHub.Application/Services/RecipeModelService.cs
@@ -0,0 +1,27 @@
+﻿namespace SatisfactorySmartHub.Application.Services;
+
+internal class RecipeModelService
+{
+    private ICollection<RecipeModel> _recipes = Recipes.RecipeList;
+
+    public decimal GetNormalizedPowerConsumtion(RecipeModel recipe)
+    {
+        decimal productAmount = recipe.MainProduct.Amount;
+        decimal machinePowerConsumption = recipe.Machine.PowerConsumption;
+
+        decimal result = 100 / productAmount * machinePowerConsumption;
+
+        return Math.Round(result, 4);
+    }
+
+    public ICollection<RecipeModel> GetMainRecipes(ItemModel model)
+    {
+        return _recipes.Where(x => x.MainProduct.Item.Name == model.Name).ToList();
+    }
+
+    public ICollection<RecipeModel> UsedRecipes
+    {
+        get => _recipes;
+        set => _recipes = value;
+    }
+}
---
+++ SatisfactorySmartHub/SatisfactorySmartHub.Domain/Models/RecipeModel.cs
@@ -0,0 +1,54 @@
+﻿using SatisfactoryCalculator.Infrastructure.Persistence.StaticDataModel;
+using SatisfactorySmartHub.Domain.Models.Enums;
+using System.Reflection.PortableExecutable;
+
+namespace SatisfactorySmartHub.Domain.Models;
+
+internal class RecipeModel
+{
+    private string _name = string.Empty;
+    private MachineModel _machine = new();
+    private ICollection<ItemWithAmount> _ingredients = new HashSet<ItemWithAmount>();
+    private ItemWithAmount _mainProduct = new();
+    private ICollection<ItemWithAmount> _byproducts = new HashSet<ItemWithAmount>();
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+    public MachineModel Machine
+    {
+        get => _machine;
+        set => _machine = value;
+    }
+    public ICollection<ItemWithAmount> Ingredients
+    {
+        get => _ingredients;
+        set => _ingredients = value;
+    }
+    public ItemWithAmount MainProduct
+    {
+        get => _mainProduct;
+        set => _mainProduct = value;
+    }
+    public ICollection<ItemWithAmount> Byproducts
+    {
+        get => _byproducts;
+        set => _byproducts = value;
+    }
+
+    public RecipeComponentType GetItemUsageInRecipe(ItemModel item)
+    {
+        if (Ingredients.Any(x => x.Item.Name == item.Name))
+            return RecipeComponentType.Ingredient;
+
+        if (MainProduct.Item.Name == item.Name)
+            return RecipeComponentType.MainProduct;
+
+        if (Byproducts.Any(x => x.Item.Name == item.Name))
+            return RecipeComponentType.ByProduct;
+
+        return RecipeComponentType.NotIncluded;
+    }
+}
