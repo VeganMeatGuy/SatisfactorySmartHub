e50f89ecb018cd3701562d8008aaa520b415f9ca
VeganMeatGuy <steudel.peter@gmail.com>
2024-06-26 00:24:59 +0200

    feature MainWindow
    added Menue - not finished!

---
+++ SatisfactorySmartHub/SatisfactorySmartHub.Presentation/Controls/MenueButton.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace SatisfactorySmartHub.Presentation.Controls;
+
+/// <summary>
+/// Interaction logic for MenueButton.xaml
+/// </summary>
+public partial class MenueButton : RadioButton
+{
+    public MenueButton()
+    {
+        InitializeComponent();
+    }
+}
