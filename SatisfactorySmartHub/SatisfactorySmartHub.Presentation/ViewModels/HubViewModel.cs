8b753b57f8745d3415332792e29005aaacea7be4
VeganMeatGuy <steudel.peter@gmail.com>
2024-07-03 18:58:36 +0200

    feature HubView
    added properties to HubViewModel and bin them to HubView

--- SatisfactorySmartHub/SatisfactorySmartHub.Presentation/ViewModels/HubViewModel.cs
+++ SatisfactorySmartHub/SatisfactorySmartHub.Presentation/ViewModels/HubViewModel.cs
@@ -1,4 +1,5 @@
-﻿using SatisfactorySmartHub.Presentation.ViewModels.Base;
+﻿using CommunityToolkit.Mvvm.Input;
+using SatisfactorySmartHub.Presentation.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,42 @@ namespace SatisfactorySmartHub.Presentation.ViewModels;
 
 public sealed class HubViewModel : ViewModelBase
 {
+
+    private IRelayCommand? _loadCompanyCommand;
+    private IRelayCommand? _createCompanyCommand;
+    private string _companyName = string.Empty;
+    private string _createHint = string.Empty;
+    private string _loadHint = string.Empty;
+
     public HubViewModel() { }
+
+    public IRelayCommand CreateCompanyCommand => _createCompanyCommand ?? new RelayCommand(new Action(CreateCompany));
+    public IRelayCommand LoadCompanyCommand => _loadCompanyCommand ?? new RelayCommand(new Action(LoadCompany));
+
+    public string CompanyName
+    {
+        get => _companyName;
+        set => SetProperty(ref _companyName, value);
+    }
+
+    public string CreateHint
+    {
+        get => _createHint;
+        set => SetProperty(ref _createHint, value);
+    }
+
+    public string LoadHint
+    {
+        get => _loadHint;
+        set => SetProperty(ref _loadHint, value);
+    }
+
+    private void CreateCompany()
+    { 
+
+    }
+    private void LoadCompany()
+    {
+
+    }
 }
