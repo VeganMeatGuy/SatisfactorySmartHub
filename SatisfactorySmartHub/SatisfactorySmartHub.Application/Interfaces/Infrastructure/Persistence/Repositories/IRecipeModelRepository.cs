c378cb1cd34e901144cc0ee970b1aff974d748bc
VeganMeatGuy <steudel.peter@gmail.com>
2024-07-05 17:44:08 +0200

    refactor services
    deleted unnecessary services

--- SatisfactorySmartHub/SatisfactorySmartHub.Application/Interfaces/Infrastructure/Persistence/Repositories/IRecipeModelRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using SatisfactorySmartHub.Domain.Models;
-
-namespace SatisfactorySmartHub.Application.Interfaces.Infrastructure.Persistence.Repositories;
-
-public interface IRecipeModelRepository
-{
-    IEnumerable<RecipeModel> GetAll();
-}
